0c0928c864a90ef516270dee9898743e89b3e3b2
John Doe <johndoe@example.com>
2024-12-16 12:55:59 +0000

    commit

--- script/setup_nfs_client.sh
+++ script/setup_nfs_client.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Configuramos para mostrar los comandos y finalizar si hay error
@@ -16,9 +15,13 @@ apt upgrade -y
 # Instalamos el cliente NFS 
 apt install nfs-common -y
 
+# Creamos la carpeta moodledata
+mkdir -p /var/moodledata
+
 #  Creamos el punto de montaje en el cliente NFS
 sudo mount $NFS_SERVER_IP:/var/www/html /var/www/html
 sudo mount $NFS_SERVER_IP:/var/moodledata /var/moodledata
 
+#/etc/fstab
 echo "$NFS_SERVER_IP:/var/www/html /var/www/html  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
 echo "$NFS_SERVER_IP:/var/moodledata /var/moodledata  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
