2555b42d6b99e2f656f02816b42ce907bc4044b9
John Doe <johndoe@example.com>
2024-12-16 12:11:06 +0000

    1ero

---
+++ script/install_lamp_frontend_moodle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+apt update
+
+#apt upgrade -y
+
+sudo apt install apache2 -y
+ 
+cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
+
+sudo apt install php php-mysql libapache2-mod-php php-xml php-mbstring php-curl php-zip php-gd php-intl php-soap -y
+
+systemctl restart apache2
+
---
+++ script/setup_letsencrypt_https.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+source .env
+
+snap install core
+snap refresh core
+
+apt remove certbot
+
+snap install --classic certbot
+
+ln -sf /snap/bin/certbot /usr/bin/certbot 
+
+certbot --apache -m $moodle_admin_email --agree-tos --no-eff-email -d $LE_DOMAIN --non-interactive
