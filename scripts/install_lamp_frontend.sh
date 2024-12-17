4e069efb94ca49cec555171d4affe630a343c9ae
John Doe <johndoe@example.com>
2024-12-17 18:50:51 +0000

    commit

--- scripts/install_lamp_frontend.sh
+++ scripts/install_lamp_frontend.sh
@@ -19,7 +19,7 @@ a2enmod rewrite
 cp ../conf/000-default.conf /etc/apache2/sites-available
 
 # Instalamos PHP y algunos módulos de php para Apache y MySQL
-apt install php libapache2-mod-php php-mysql -y
+apt install php libapache2-mod-php php-mysql php-xml php-mbstring php-curl php-zip php-gd php-intl php-soap -y
 
 # Reiniciamos el servicio de Apache
 systemctl restart apache2
@@ -29,4 +29,3 @@ cp ../php/index.php /var/www/html
 
 # Modificamos el propietario y el grupo del archivo index.php
 chown -R www-data:www-data /var/www/html
-
--- scripts/setup_loadbalancer.sh
+++ scripts/setup_loadbalancer.sh
@@ -16,20 +16,20 @@ apt upgrade -y
 apt install nginx -y
 
 # Deshabilitamos el virtualhost por defecto
-if [ -f "/etc/nginx/sites-enabled/default"]; then
-unlink /etc/nginx/sites-enabled/default
+if [ -f "/etc/nginx/sites-enabled/default" ]; then
+    unlink /etc/nginx/sites-enabled/default
 fi
 # Copiamos el archivo de configuración de Nginx
-cp ../conf/load-balancer.conf /etc/nginx/sites-available
+cp ../conf/loadbalancer.conf /etc/nginx/sites-available
 
 # Sustituimos los valores de la plantilla del archivo de configuración
-sed -i "s/IP_FRONTEND_1/$IP_FRONTEND_1/" /etc/nginx/sites-available/load-balancer.conf
-sed -i "s/IP_FRONTEND_2/$IP_FRONTEND_2/" /etc/nginx/sites-available/load-balancer.conf
-sed -i "s/LE_DOMAIN/$LE_DOMAIN/" /etc/nginx/sites-available/load-balancer.conf
+sed -i "s/IP_FRONTEND_1/$IP_FRONTEND_1/" /etc/nginx/sites-available/loadbalancer.conf
+sed -i "s/IP_FRONTEND_2/$IP_FRONTEND_2/" /etc/nginx/sites-available/loadbalancer.conf
+sed -i "s/LE_DOMAIN/$LE_DOMAIN/" /etc/nginx/sites-available/loadbalancer.conf
 
 # Habilitamos el virtualhost del balanceador de carga
-if [ ! -f "/etc/nginx/sites-enabled/load-balancer.conf" ]; then 
-ln -s /etc/nginx/sites-available/load-balancer.conf /etc/nginx/sites-enabled
+if [ ! -f "/etc/nginx/sites-enabled/loadbalancer.conf" ]; then 
+    ln -s /etc/nginx/sites-available/loadbalancer.conf /etc/nginx/sites-enabled
 fi
 
 # Reinciamos el balanceador de carga
